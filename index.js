856402377006f7babaf24a16a06c5934f9b8959c
Kaif-Zubaer <kaifhossainzubaer@gmail.com>
2025-11-09 20:58:25 +0600

    mongoBD connection

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zn65wpu.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
 app.use(cors());
 app.use(express.json());
@@ -9,6 +21,17 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('WELCOME TO ECO TRACK SERVER')
 })
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`EcoTrack server is running on port: ${port}`);
